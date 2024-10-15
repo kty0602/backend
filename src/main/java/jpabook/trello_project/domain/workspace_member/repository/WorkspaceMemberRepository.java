1f4b6e3cd0928659ace15c9278b59aa52e293ad7
inseooo0 <calmandpassion@korea.ac.kr>
2024-10-15 13:11:13 +0900

    [fix] WorkspaceMemberRepository method fix

--- src/main/java/jpabook/trello_project/domain/workspace_member/repository/WorkspaceMemberRepository.java
+++ src/main/java/jpabook/trello_project/domain/workspace_member/repository/WorkspaceMemberRepository.java
@@ -1,12 +1,13 @@
 package jpabook.trello_project.domain.workspace_member.repository;
 
 import jpabook.trello_project.domain.user.entity.User;
+import jpabook.trello_project.domain.workspace.entity.Workspace;
 import jpabook.trello_project.domain.workspace_member.entity.WorkspaceMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface WorkspaceMemberRepository extends JpaRepository<WorkspaceMember, Long> {
-    Optional<WorkspaceMember> findByUser(User user);
-    boolean existsByUser(User user);
+    Optional<WorkspaceMember> findByUserAndWorkspace(User user, Workspace workspace);
+    boolean existsByUserAndWorkspace(User user, Workspace workspace);
 }
