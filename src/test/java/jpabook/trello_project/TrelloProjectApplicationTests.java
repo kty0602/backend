eeaa719005d0200f4e617cf06eb1ba227cf03d5a
areum0116 <areum0116@naver.com>
2024-10-18 11:50:27 +0900

    [docs] Add Dockerfile, remove test code

--- src/test/java/jpabook/trello_project/TrelloProjectApplicationTests.java
+++ src/test/java/jpabook/trello_project/TrelloProjectApplicationTests.java
@@ -1,13 +1,17 @@
-// package jpabook.trello_project;
-
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-
-// @SpringBootTest
-// class TrelloProjectApplicationTests {
-
-//     @Test
-//     void contextLoads() {
-//     }
-
-// }
+//package jpabook.trello_project;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
+//
+//@TestPropertySource(properties = {"spring.profiles.active=test"})
+//@AutoConfigureMockMvc(addFilters = false) // 필터 비활성화
+//@SpringBootTest
+//class TrelloProjectApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
